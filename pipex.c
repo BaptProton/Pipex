6a071ea1c3de8dadd09a9daddfe9d879cd1ee981
Baptiste Proton <bproton@c1r17s4.42lausanne.ch>
2024-04-09 16:09:53 +0200

    str parse

--- pipex.c
+++ pipex.c
@@ -3,14 +3,24 @@
 /*                                                        :::      ::::::::   */
 /*   pipex.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: proton <proton@student.42.fr>              +#+  +:+       +#+        */
+/*   By: bproton <bproton@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/04 17:38:16 by proton            #+#    #+#             */
-/*   Updated: 2024/04/09 09:41:28 by proton           ###   ########.fr       */
+/*   Updated: 2024/04/09 15:59:46 by bproton          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
+
+int	parse_arguments(char *arg1, char *arg2)
+{
+	char	**str1;
+	char	**str2;
+
+	str1 = ft_split(arg1, ' ');
+	str2 = ft_split(arg2, ' ');
+	
+}
 
 char	*open_input_file(const char *str)
 {
@@ -19,6 +29,11 @@ char	*open_input_file(const char *str)
 	int		check;
 
 	fd = open(str, O_RDONLY);
+	if (fd < 0)
+	{
+		perror("1");
+		return (NULL);
+	}
 	new = malloc(sizeof(char) * BUFFERSIZE);
 	if (!new)
 		return (NULL);
@@ -36,12 +51,16 @@ int	main(int argc, char **argv)
 {
 	char	*input;
 
-	if (argc == 2)
+	if (argc == 4)
 	{
 		input = open_input_file(argv[1]);
 		if (!input)
 			return (1);
-		get_token
+		if (!parse_arguments(argv[3], argv[4]));
+		{
+			free(input);
+			return (1);
+		}
 	}
 	else
 		(void)input;
