e9d7b19bbb1c10d368b0cff2d3cc4814684b54f6
Proton <147440352+Madcongroo@users.noreply.github.com>
2024-04-12 08:59:23 +0200

    nothing new

--- pipex.c
+++ pipex.c
@@ -6,7 +6,7 @@
 /*   By: proton <proton@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/04 17:38:16 by proton            #+#    #+#             */
-/*   Updated: 2024/04/10 18:24:07 by proton           ###   ########.fr       */
+/*   Updated: 2024/04/12 08:43:06 by proton           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,28 @@
 
 int	parse_arguments(char *input, char *arg, char *output, char **envp)
 {
-	int	fd[2];
+	int		fd;
+	int		pid;
+	char	**n_arg;
 
 	pipe(fd);
+	pid = fork();
+	n_arg = ft_split(arg, ' ');
+	if (pid == -1)
+		return (1);
+	if (pid == 0)
+	{
+		fd = open(input, O_RDONLY);
+		dup2(fd, STDIN_FILENO);
+	}
+	else
+	{
+		wait(pid);
+		execve(input, n_arg, envp);
+		
+	}
+	pipe(fd);
+	if ()
 }
 
 int	main(int argc, char **argv, char **envp)
